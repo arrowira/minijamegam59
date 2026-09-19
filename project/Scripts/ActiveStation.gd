f8195f97a4c8a4d78db10ff561ba72d2692dcfcd
arrowira <41136800+arrowira@users.noreply.github.com>
2026-09-19 00:36:12 -0700

    progress bar logic

--- project/Scripts/ActiveStation.gd
+++ project/Scripts/ActiveStation.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+var inPlayer = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,4 +8,17 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	$progress.value = $progressTimer.time_left/$progressTimer.wait_time
+	if inPlayer and Input.is_action_just_pressed("interact"):
+		$progressTimer.start()
+	if inPlayer and Input.is_action_just_released("interact"):
+		$progressTimer.reset()
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.name == "PlayerArea":
+		inPlayer = true
+
+
+func _on_area_2d_area_exited(area: Area2D) -> void:
+	if area.name == "PlayerArea":
+		inPlayer = false
