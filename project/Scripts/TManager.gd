81646bae8229b272b4e503d06eff221c581c16c5
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2026-09-19 00:16:52 -0700

    Tomtao

--- project/Scripts/TManager.gd
+++ project/Scripts/TManager.gd
@@ -6,5 +6,5 @@ func _ready():
 	TScale = $Sprite.scale
 	var tween = get_tree().create_tween()
 	tween.set_loops()
-	tween.tween_property($Sprite, "scale", Vector2(TScale * 1.1), 2.5)
-	tween.tween_property($Sprite, "scale", Vector2(TScale * 0.9), 2.5)
+	tween.tween_property($Sprite, "scale", Vector2(TScale * 1.05), 2.5)
+	tween.tween_property($Sprite, "scale", Vector2(TScale * 0.95), 2.5)
