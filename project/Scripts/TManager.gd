10feb606370595ba8b26e0adc13cd2f48d341542
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2026-09-19 00:22:10 -0700

    Tomato Fix

--- project/Scripts/TManager.gd
+++ project/Scripts/TManager.gd
@@ -1,10 +1,22 @@
 extends Node2D
 
 var TScale = Vector2.ZERO
+var Grab = true
+var ToGrab = 0
+var IdleTween
 
 func _ready():
+	IdleTween = get_tree().create_tween()
 	TScale = $Sprite.scale
-	var tween = get_tree().create_tween()
-	tween.set_loops()
-	tween.tween_property($Sprite, "scale", Vector2(TScale * 1.05), 2.5)
-	tween.tween_property($Sprite, "scale", Vector2(TScale * 0.95), 2.5)
+	IdleTween.set_loops()
+	IdleTween.tween_property($Sprite, "scale", Vector2(TScale * 1.05), 2.5)
+	IdleTween.tween_property($Sprite, "scale", Vector2(TScale * 0.95), 2.5)
+	
+func _physics_process(delta: float) -> void:
+	if(Grab == false):
+		IdleTween.pause()
+		ToGrab +=1
+		if(ToGrab >= 120):
+			ToGrab = 0
+			Grab = true
+			IdleTween.play()
