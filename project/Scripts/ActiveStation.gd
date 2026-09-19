cdfa92db03f8667ba7c7bf25c03b2ef40912bf42
arrowira <41136800+arrowira@users.noreply.github.com>
2026-09-19 00:49:20 -0700

    cutting board progress bar

--- project/Scripts/ActiveStation.gd
+++ project/Scripts/ActiveStation.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var inPlayer = false
+var active = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,13 +9,20 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$progress.value = $progressTimer.time_left/$progressTimer.wait_time
+	if !active:
+		$progress.value = 0
+	else:
+		$progress.value = 1-$progressTimer.time_left/$progressTimer.wait_time
 	if inPlayer and Input.is_action_just_pressed("interact"):
 		$progressTimer.start()
+		active = true
 	if inPlayer and Input.is_action_just_released("interact"):
-		$progressTimer.reset()
+		active = false
+		$progressTimer.stop()
+		
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
+	print(area.name)
 	if area.name == "PlayerArea":
 		inPlayer = true
 
